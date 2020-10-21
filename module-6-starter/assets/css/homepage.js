618b798e93693619f1ca68407376e613fceb3559
Juan <junehmlv98@gmail.com>
2020-10-20 18:23:01 -0700

    updated with new "fetch" api

--- module-6-starter/assets/css/homepage.js
+++ module-6-starter/assets/css/homepage.js
@@ -1,7 +1,23 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
+
+console.log("outside");
+
+
     console.log("function was called");
 
   
-  getUserRepos();
+  getUserRepos("microsoft"); {
+    var response = fetch("https://api.github.com/users/octocat/repos");
+    console.log(response);
+  }
+  
